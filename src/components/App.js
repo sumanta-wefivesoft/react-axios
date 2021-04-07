20fff749e6c508f4ba5b5533e16b090907fae792
sumanta-jana <sumanta.jana@wefivesoft.com>
2021-04-07 07:48:21 +0530

    commit1

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    componentDidMount() { }
+    render() {
+        return <div className="App">Hello ReactJS</div>
+    }
+}
+export default App;
